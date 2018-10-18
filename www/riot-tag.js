88426a92d78c0b39c723d3be0a6aad0cc99d60d3
starekrow <starekrow@metalcoder.com>
2018-10-17 21:31:13 -0700

    more loading stuff

--- www/riot-tag.js
+++ www/riot-tag.js
@@ -10,30 +10,48 @@ define(function(require, exports, module) {
 
         tagparser: /<([a-z][a-z0-9]*-[-a-z0-9]+)[^>]*>/,
 
-        load: function (name, req, onLoad, config) {
+        loadRiotTag: function(tag, loaded) 
+        {
+            require("text!tags/" + tag, function(tagdef) {
+                var r = riot.compile(tagdef);
+                loaded(r);    
+            }, function(err) {
+                loaded(null);
+            });
+        }
+
+        load: function (name, req, onLoad, config) 
+        {
             req(["text!tags/" + name + ".tag"], function(tagdef) {
                 // find any custom tags used in this tag, define them
                 // define the tag
                 var p = riot_tag.tagparser;
+                var loaders;
                 p.lastIndex = 0;
-                var handler = function(r) {
-
-                }
-                for(var got;(got = p.exec(tagdef));) {
-                    if (!riot_tag.loaded[got]) {
-                        riot_tag.loaded[got] = true;
-                        loaders[got] = new Promise(function(res, rej) {
-                            require("text!" + got, function(tag) {
-                                res(tag);
-                            }, function(err) {
-
-                            });
-                        });
+                var mkloader = function(tag) {
+                    return new Promise(function(res, rej) {
+                        require("riot-tag!" + tag, function(tag) {
+                            
+                        })
+                        riot_tag.loadRiotTag(got, res);
+                    });
+                };
+                for(let tag;(tag = p.exec(tagdef));) {
+                    if (!riot_tag.loaded[tag]) {
+                        riot_tag.loaded[tag] = true;
+                        loaders.push(mkloader(tag));
                     }
                 }
                 Promise.all(loaders).then(function() {
-                    var r = riot.compile(tagdef);
-                    onLoad(r);    
+                    riot_tag.loadRiotTag(got, function(tag) {
+
+                    })                    
+                    require("text!" + got, function(tag) {
+                        var r = riot.compile(tagdef);
+                        onLoad(r);    
+                    }, function(err) {
+                        res(null);
+                    });
                 });
                 riot_tag.tags
             }, function(err) {
