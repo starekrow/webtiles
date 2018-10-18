b2873080c0a47efe823c6393ff752670da732271
starekrow <starekrow@metalcoder.com>
2018-10-17 20:23:39 -0700

    more loading stuff

--- www/riot-tag.js
+++ www/riot-tag.js
@@ -5,15 +5,37 @@ define(function(require, exports, module) {
 
     var masterConfig = (module.config && module.config()) || {};
 
-    riot_tag = {
+    var riot_tag = {
         version: "0.0.1",
+
+        tagparser: /<([a-z][a-z0-9]*-[-a-z0-9]+)[^>]*>/,
 
         load: function (name, req, onLoad, config) {
             req(["text!tags/" + name + ".tag"], function(tagdef) {
                 // find any custom tags used in this tag, define them
                 // define the tag
-                r = riot.mount(tagdef);
-                onLoad( r );
+                var p = riot_tag.tagparser;
+                p.lastIndex = 0;
+                var handler = function(r) {
+
+                }
+                for(var got;(got = p.exec(tagdef));) {
+                    if (!riot_tag.loaded[got]) {
+                        riot_tag.loaded[got] = true;
+                        loaders[got] = new Promise(function(res, rej) {
+                            require("text!" + got, function(tag) {
+                                res(tag);
+                            }, function(err) {
+
+                            });
+                        });
+                    }
+                }
+                Promise.all(loaders).then(function() {
+                    var r = riot.compile(tagdef);
+                    onLoad(r);    
+                });
+                riot_tag.tags
             }, function(err) {
                 // hmm, didn't load
                 onLoad.error(err);
